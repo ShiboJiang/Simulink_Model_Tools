88bc27b3d6b823154860bc64363ba83f33a83bdb
Tomato666 <353838430@qq.com>
2018-01-06 17:28:18 +0800

    更新 change_port_property.m

--- change_port_property.m
+++ change_port_property.m
@@ -2,8 +2,8 @@
 %   Simulink scrip for change ports property
 %   MATLAB version: R2017a
 %   Author        : Shibo Jiang 
-%   Version       : 0.5
-%   Time          : 2017/11/2
+%   Version       : 0.6
+%   Time          : 2018/1/6
 %   Instructions  : fix bugs
 %         修改bug，仅更改模型root层的输入和输出端口属性，不对子系统进行修改  - 0.2
 %         修改脚本，在遇到线上没有数据类型端口，不进行默认为uint8的类型改写，
@@ -12,7 +12,10 @@
 %         修改bug，在检测到模型配置中采样时间设为连续时，将端口的采样时间设
 %         为 默认的 -1                                                  - 0.4  
 %         修改bug，当设为不连续采样，但是采样时间设为auto时，将端口采样时间
-%         设为默认的 -1                                                  - 0.5                          
+%         设为默认的 -1                                                  - 0.5 
+%         修复bug，当输入输出端口连接线上没有定义信号名称的时候，之前的版本
+%         无法修改该端口的属性值，当前版本改掉了这个bug;
+%         增加自定义数据类型uint8...float32等识别;                          - 0.6                         
 %------------------------------------------------------------------------------
 
 function change_ports_result = change_port_property()
@@ -64,7 +67,8 @@ function ChangePortProperty(paraModel, block)
         % translate cell to string
         current_out_sig_name = cell2mat(current_out_sig_name);
         if 1 == isempty(current_out_sig_name)
-            % Do nothing. output line has no name defined.       
+            % Only try change property. output line has no name defined.
+            set_datatype = 'byblock';      
         else
             % find if tag symbol '<' exit
             if strcmp('<',current_out_sig_name(1))
@@ -72,55 +76,74 @@ function ChangePortProperty(paraModel, block)
                 current_out_sig_name = current_out_sig_name(2:end-1);
             else
                 % Do nothing
-            end
-            % change ports property
-            try
-                set(inport_block(i),'Name',current_out_sig_name);    
+            end                
+            % change ports property and name
+            try    
+                set(inport_block(i),'Name',current_out_sig_name);
                 % get parameters datatype
                 inport_datatype = current_out_sig_name(end-2:end);
                 [set_min, set_max,...
                  set_datatype] = CalculateDataType(inport_datatype);
-                disable_flag = 0;
-                if strcmp('None',set_datatype)
-                    set_datatype = get(inport_block(i),'OutDataTypeStr');
-                    set_datatype = TranslateDatatype(set_datatype);
-                    if strcmp('StillNone',set_datatype)
-                        % do not set port's property
-                        disable_flag = 1;
-                    else
-                        % get new set value
-                        [set_min, set_max, ...
-                        set_datatype] = CalculateDataType(set_datatype);
-                    end
-                else
-                    % do nothing
-                end
-                % judge whether set port's property
-                if 0 == disable_flag
-                    % get cfg
-                    if strcmp('Fixed-step', get_param(paraModel, 'SolverType'))
-                        sample_time = get_param(paraModel,'FixedStep');
-                        % Fix bug , step set Auto and use fixed-step mode
-                        if strcmp('auto', sample_time)
-                            sample_time = '-1';
-                        end
-                    else
+            catch
+                % Do nothing           
+            end
+        end
+        % Calculate correct datatype
+        disable_flag = 0;
+        if strcmp('None',set_datatype)
+            set_datatype = get(inport_block(i),'OutDataTypeStr');
+            set_datatype = TranslateDatatype(set_datatype);
+            if strcmp('StillNone',set_datatype)
+                % do not set port's property
+                disable_flag = 1;
+            else
+                % get new set value
+                [set_min, set_max, ...
+                set_datatype] = CalculateDataType(set_datatype);
+            end
+
+        elseif strcmp('byblock',set_datatype)
+            set_datatype = get(inport_block(i),'OutDataTypeStr');
+            set_datatype = TranslateDatatype(set_datatype);
+            if strcmp('StillNone',set_datatype)
+                % do not set port's property
+                disable_flag = 1;
+            else
+                % get new set value
+                [set_min, set_max, ...
+                ~] = CalculateDataType(set_datatype);
+                set_datatype = get(inport_block(i),'OutDataTypeStr');
+            end
+        else
+            % do nothing
+        end
+        % Set property
+        try
+            % judge whether set port's property
+            if 0 == disable_flag
+                % get cfg
+                if strcmp('Fixed-step', get_param(paraModel, 'SolverType'))
+                    sample_time = get_param(paraModel,'FixedStep');
+                    % Fix bug , step set Auto and use fixed-step mode
+                    if strcmp('auto', sample_time)
                         sample_time = '-1';
                     end
-                    % define inport block property
-                    port_dimension = '1';
-                    SetPortProperty(inport_block(i), set_min, set_max,...
-                                    set_datatype, sample_time, port_dimension) 
-                    % clear port signal resolved
-                    inport_signal_line = find_system(paraModel,'FindAll',...
-                             'on','type','line','Name',current_out_sig_name);  
-                    set(inport_signal_line,'MustResolveToSignalObject',0); 
                 else
-                    % do nothing  
-                end  
-            catch
-            
-            end
+                    sample_time = '-1';
+                end
+                % define inport block property
+                port_dimension = '1';
+                SetPortProperty(inport_block(i), set_min, set_max,...
+                                set_datatype, sample_time, port_dimension) 
+                % clear port signal resolved
+                inport_signal_line = find_system(paraModel,'FindAll',...
+                            'on','type','line','Name',current_out_sig_name);  
+                set(inport_signal_line,'MustResolveToSignalObject',0); 
+            else
+                % do nothing  
+            end  
+        catch
+        
         end
     end
 end
@@ -185,15 +208,15 @@ function set_datatype = TranslateDatatype(datatype)
         set_datatype = 'u16';
     case 'uint32'
         set_datatype = 'u32';
-    case 'single'
+    case {'single', 'float32'}
         set_datatype = 'f32';
-    case 'double'
+    case {'double', 'float64'}
         set_datatype = 'f64';
-    case 'int8'
+    case {'int8', 'sint8'}
         set_datatype = '_s8';
-    case 'int16'
+    case {'int16', 'sint16'}
         set_datatype = 's16';
-    case 'int32'
+    case {'int32', 'sint32'}
         set_datatype = 's32';
     case 'boolean'
         set_datatype = '_bl';
